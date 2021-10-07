8d7b0e9066153752f127301fa535cc6190101506
Sonya Brazell <sonyabrazell@Sonyas-MacBook-Air.local>
2021-10-07 16:21:40 -0500

    Removed additional pages.

--- app.py
+++
@@ -1,7 +0,0 @@
-import destinations, entertainment, restaurants, transportation, random
-
-def trip_generator(destination):
-    destination = destinations.final_destination(0,3)
-    print(destination)
-    return destination
-
